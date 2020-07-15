67ee971603887144d6a93410aa1ee56fdac28946
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-07-16 01:08:38 +0800

    Creating a basic table dict as what I thought

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pandas as pd
+from datetime import datetime
 
-filename = "test.csv"
+# First, read the csv file into a dict as `data`
+csv_file = "test.csv"
+df = pd.read_csv(csv_file, encoding="gbk")
 
-df = pd.read_csv(filename, encoding="gbk")
-
-df_modified = df[df.columns[:-1]]
-df_modified.columns = df.columns[1:]
-df = df_modified
-
+# to get rid of the last column
+data = df[df.columns[:-1]]
+data.columns = df.columns[1:]
 # print(df.to_html(classes=['table', 'table-striped']))
+
+
+table_dict = {}
+today = datetime.now()
+weekday_table = ['一', '二', '三', '四', '五', '六', '日']
+
+table_dict["month"] = {'{}月'.format(today.month): str(today.day)}
+table_dict["weekday"] = {'星期': weekday_table[today.weekday()]}
+table_dict["wether"] = {'天气': None}
+var = 10 # just for testing now
+table_dict["all"] = {'营业额': var}
