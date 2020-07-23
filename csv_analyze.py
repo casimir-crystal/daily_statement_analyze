00cfa062cb6f68e9fd5926f8d3ad3585600d99fd
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-07-23 20:57:25 +0800

    Added features, improved functions

--- csv_analyze.py
+++ csv_analyze.py
@@ -7,11 +7,12 @@ def table_export(csv_filename, form_dict, today=datetime.now()):
     完整的table构建函数。接受由收银机生成并由用户上传的csv文件名为参数。
     """
 
-    def float_or_int(s):
-        if '.' in str(s):
-            return round(float(s), 2)
+    def float_or_int(num):
+        num = float(num)
+        if int(num) == num:
+            return int(num)
         else:
-            return int(s)
+            return num // 0.01 / 100
 
     # First, read the csv file into a dict as `data`
     with open(csv_filename, encoding="gbk") as f:
@@ -47,17 +48,20 @@ def table_export(csv_filename, form_dict, today=datetime.now()):
     table_dict['商场'] = ''
     table_dict['POS机GC'] = ''
     table_dict['POS机AC'] = ''
-    table_dict['小程序'] = float_or_int(csv_dict['开个店支付']['总金额(元)'])
-    table_dict['小程序GC'] = int(float(csv_dict['开个店支付']['单数']))
+    table_dict['小程序'] = float_or_int(form_dict['xiaochengxu_turnover'])
+    table_dict['小程序GC'] = float_or_int(form_dict['xiaochengxu_sells'])
     table_dict['小程序AC'] = float_or_int(table_dict['小程序'] / table_dict['小程序GC'])
     table_dict['线下合计'] = table_dict['现金'] + table_dict['微信'] + table_dict['支付宝'] + table_dict['口碑掌柜'] + table_dict['开店宝(美团)'] + table_dict['小程序']
     table_dict['线下GC'] = sum(map(int, [csv_dict[key]['单数'] for key in ['现金支付', '微信支付', '支付宝支付', '开个店支付']])) + int(form_dict['kaidianbao_sells']) + int(form_dict['koubei_sells'])
-    table_dict['线下AC'] = ''
-    table_dict['储蓄卡/福利券'] = ''
+    table_dict['线下AC'] = float_or_int(table_dict['线下合计'] / table_dict['线下GC'])
+    table_dict['储蓄卡/福利券'] = float_or_int(csv_dict['会员卡支付']['总金额(元)'])
     table_dict['签名'] = ''
 
     table_dict['营业额'] = table_dict['线上合计'] + table_dict['线下合计']
     table_dict['GC'] = table_dict['线上GC'] + table_dict['线下GC']
     table_dict['AC'] = float_or_int(table_dict['营业额'] / table_dict['GC'])
+
+    table_dict['累计营业额'] = table_dict['营业额'] + 0
+    table_dict['累计GC'] = table_dict['GC'] + 0
 
     return table_dict
