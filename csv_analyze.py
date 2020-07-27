946b6ca43e9b12cf7f8300d611649bbdbda6ec19
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-07-27 12:05:01 +0800

    added TypeError to float()'s exception

--- csv_analyze.py
+++ csv_analyze.py
@@ -18,7 +18,7 @@ def table_export(csv_filename, form_dict, today=datetime.now()):
                     # value = value // 0.01 / 100  # 保留两位小数
                     # value = round(value, 2)  # 四舍五入，奇进偶舍：https://www.cnblogs.com/xieqiankun/p/the_truth_of_round.html
                     value = Decimal(str(value)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)  # 通常的四舍五入
-            except ValueError:
+            except ValueError, TypeError:
                 # it's not a number, that's fine
                 pass
             finally:
