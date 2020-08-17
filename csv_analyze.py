ff69a8cd493865dfa1ad56a6ba699c1d43de7e92
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-08-17 17:42:55 +0800

    Fixed class name typo; Changed project name

--- csv_analyze.py
+++ csv_analyze.py
@@ -13,7 +13,8 @@ class NumbericDict(UserDict):
             else:
                 # value = value // 0.01 / 100  # 保留两位小数
                 # value = round(value, 2)  # 四舍五入，奇进偶舍：https://www.cnblogs.com/xieqiankun/p/the_truth_of_round.html
-                value = float(Decimal(str(value)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP))  # 通常的四舍五入
+                value = float(Decimal(str(value)).quantize(
+                    Decimal('0.00'), rounding=ROUND_HALF_UP))  # 通常的四舍五入
         except (ValueError, TypeError):
             # it's not a number, that's fine
             pass
