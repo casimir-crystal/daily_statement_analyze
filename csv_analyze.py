b00ddfaf4d6ebf05b677823d695cc6478f668527
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-08-25 01:36:26 +0800

    No need to ask user for  xiaochengxu data

--- csv_analyze.py
+++ csv_analyze.py
@@ -1,6 +1,5 @@
 import csv
 from datetime import datetime
-from decimal import Decimal, ROUND_HALF_UP
 from collections import UserDict
 
 
@@ -11,10 +10,8 @@ class NumbericDict(UserDict):
             if int(value) == value:
                 value = int(value)
             else:
-                # value = value // 0.01 / 100  # 保留两位小数
+                value = value // 0.01 / 100  # 保留两位小数
                 # value = round(value, 2)  # 四舍五入，奇进偶舍：https://www.cnblogs.com/xieqiankun/p/the_truth_of_round.html
-                value = float(Decimal(str(value)).quantize(
-                    Decimal('0.00'), rounding=ROUND_HALF_UP))  # 通常的四舍五入
         except (ValueError, TypeError):
             # it's not a number, that's fine
             pass
@@ -60,11 +57,11 @@ def table_export(csv_filename, form_dict, last_result=None, today=datetime.now()
     table_dict['商场'] = ''
     table_dict['POS机GC'] = ''
     table_dict['POS机AC'] = ''
-    table_dict['小程序'] = form_dict['xiaochengxu_turnover']
-    table_dict['小程序GC'] = form_dict['xiaochengxu_sells']
+    table_dict['小程序'] = csv_dict['开个店支付']['总金额(元)']
+    table_dict['小程序GC'] = csv_dict['开个店支付']['单数']
     table_dict['小程序AC'] = table_dict['小程序'] / table_dict['小程序GC']
     table_dict['线下合计'] = table_dict['现金'] + table_dict['微信'] + table_dict['支付宝'] + table_dict['口碑掌柜'] + table_dict['开店宝(美团)'] + table_dict['小程序']
-    table_dict['线下GC'] = sum(map(int, [csv_dict[key]['单数'] for key in ['现金支付', '微信支付', '支付宝支付']])) + int(form_dict['xiaochengxu_sells'])
+    table_dict['线下GC'] = sum(int(csv_dict[key]['单数']) for key in ['现金支付', '微信支付', '支付宝支付', '开个店支付'])
     table_dict['线下AC'] = table_dict['线下合计'] / table_dict['线下GC']
     table_dict['储蓄卡/福利券'] = csv_dict['会员卡支付']['总金额(元)']
     table_dict['签名'] = ''
