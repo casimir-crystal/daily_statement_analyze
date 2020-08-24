77735aadb3265fa4efb0cade70ea02b1b2d75240
Casimir Crystal <casimir.crystal.blue@outlook.com>
2020-08-25 02:00:46 +0800

    rewrite the calc function to "HALF_ROUND_UP"

--- csv_analyze.py
+++ csv_analyze.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import datetime
 from collections import UserDict
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class NumbericDict(UserDict):
@@ -12,6 +13,7 @@ class NumbericDict(UserDict):
             else:
                 value = value // 0.01 / 100  # 保留两位小数
                 # value = round(value, 2)  # 四舍五入，奇进偶舍：https://www.cnblogs.com/xieqiankun/p/the_truth_of_round.html
+                value = float(Decimal(str(value)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP))  # 通常的四舍五入
         except (ValueError, TypeError):
             # it's not a number, that's fine
             pass
